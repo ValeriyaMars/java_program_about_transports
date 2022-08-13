47066916ed1f4fd3975637f9da9827ef3260a845
valeriya.solovova <valeriya.solovova@simbirsoft.com>
2022-08-14 00:25:30 +0400

    Создала class Cars, добавила в метод main примеры

---
+++ src/ru/marshenina/Main.java
@@ -0,0 +1,27 @@
+package ru.marshenina;
+
+public class Main {
+    public static void main(String[] args) {
+        Cars car = new Cars("Volvo",50000, "White");
+        car.go(50000);
+        car.allAboutCar();
+        car.technicalInspection();
+        car.go(50000);
+        car.allAboutCar();
+        car.technicalInspection();
+
+
+        Cars car2 = new Cars("Lada",100000,"Black");
+        car2.allAboutCar();
+        car2.technicalInspection();
+        car2.go(100000);
+        car2.allAboutCar();
+        car2.setColor("Green");
+        car2.technicalInspection();
+        car2.allAboutCar();
+
+        Cars car3 = new Cars("Hyundai", 0,"Blue");
+        car3.countDistanceBeforeTechnicalInspection(1000, 2000);
+    }
+
+}
